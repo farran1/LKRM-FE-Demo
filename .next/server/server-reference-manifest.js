fc0d39460d429835b94811f44dfe364a698fe870
andrew-nettaworks <andrew@nettaworks.com>
2025-08-01 19:37:51 -0400

    Complete working demo with TypeScript fixes and all source files

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"y+pHFEl8K1lwGqag4ldygDaHU0AQU+XrGwYn1VDsjUU=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
